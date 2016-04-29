bb31fc3dc6a492dbced4f0233008b91f9c6b5a06
wanghongxin492 <wanghongxin492@emao.com>
2016-04-29 19:19:50 +0800

    加入了校验规则

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,98 @@
-//表单模块
+// 表单模块
 var editableTable=new EditableTable({
 	jqWrapper:jQuery('#bang-gift'),
 });
 editableTable.init();
+// 
+// var jqMaybeRule = $('#maybeRule');
+    // jqMaybeRule.on('click', '.submit', function() {
+    //     var data = jqMaybeRule.serialize();
+    //     getUrl({
+    //         url: 'test/prize.json',
+    //         data: data
+    //     }).
+    //     then(function(remote) {
+    //         console.log(remote)
+    //         if (remote.code == 0) {
+
+    //         }
+    //     });
+    // });
+    // jqMaybeRule
+    //     .bootstrapValidator({
+    //         message: 'This value is not valid',
+    //         fields: {
+    //             starttime: {
+    //                 message: '容我考虑一分钟',
+    //                 validators: {
+    //                     notEmpty: {
+    //                         message: '不能为空'
+    //                     },
+    //                     stringLength: {
+    //                         min: 1,
+    //                         max: 4,
+    //                         message: '数字必须在有效范围内'
+    //                     },
+    //                     /*remote: {
+    //                         url: 'remote.php',
+    //                         message: 'The username is not available'
+    //                     },*/
+    //                     regexp: {
+    //                         regexp: /^\d+$/,
+    //                         message: '必须是数字'
+    //                     }
+    //                 }
+    //             },
+    //             endtime: {
+    //                 message: '容我考虑一分钟',
+    //                 validators: {
+    //                     notEmpty: {
+    //                         message: '不能为空'
+    //                     },
+    //                     stringLength: {
+    //                         min: 1,
+    //                         max: 4,
+    //                         message: '数字必须在有效范围内'
+    //                     },
+    //                     /*remote: {
+    //                         url: 'remote.php',
+    //                         message: 'The username is not available'
+    //                     },*/
+    //                     regexp: {
+    //                         regexp: /^\d+$/,
+    //                         message: '必须是数字'
+    //                     }
+    //                 }
+    //             },
+    //             prizecount: {
+    //                 validators: {
+    //                     notEmpty: {
+    //                         message: ''
+    //                     },
+    //                     regexp: {
+    //                         regexp: /^\d+$/,
+    //                         message: '必须是数字'
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     })
+    //     .on('success.form.bv', function(e) {
+    //         console.log(e)
+    //         // Prevent form submission
+    //         e.preventDefault();
+
+    //         // Get the form instance
+
+    //         // Get the BootstrapValidator instance
+    //         var bv = jQuery.data('bootstrapValidator');
+    //         console.log(bv);
+
+    //         // Use Ajax to submit form data
+    //         getUrl({
+    //             url:'test/json',
+    //             data:jqMaybeRule.serialize()
+    //         }).then(function(remote){
+    //             console.log(remote)
+    //         })
+    //     });
