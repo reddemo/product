8df71997f6cbd887e2d843f70572751f83142303
wanghongxin492 <wanghongxin492@emao.com>
2016-04-29 10:50:20 +0800

    table

--- src/script/render.js
+++ src/script/render.js
@@ -2,10 +2,7 @@ var renderTr=_.template(jQuery('#tr').html());
 var renderTable=_.template(jQuery('#tbody').html());
 var render=function recur(table){
 	return _.reduce(table,function(c,v,k,e){
-		if(!v.children){
-			return c+renderTr(v);
-		}else{
-			return c+renderTr(v)+recur(v.children);
-		}
-	},'');
+		return v.children?(c+renderTr(v)+recur(v.children)):
+						  (c+renderTr(v))
+	},'');		
 };
