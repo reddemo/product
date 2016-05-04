31003a96ed67e0785bf5ad66f88a9b11422beb39
wanghongxin492 <wanghongxin492@emao.com>
2016-05-04 19:12:02 +0800

    steping

--- src/script/app.js
+++ src/script/app.js
@@ -1,62 +1,71 @@
 //表单模块
-var editableTable = new EditableTable({
-    jqWrapper: jQuery('#bang-gift'),
-});
-editableTable.init();
 
-var render = _.template($('#formhtml').html());
-var markup = render({
-    name: 'mb',
-    text: '手机号',
-    attrs: [{
-        name: 'required',
-        text: '必填',
-        value: ''
-    }, {
-        name: 'required',
-        text: '必填',
-        value: ''
-    }, {
-        name: 'required',
-        text: '必填',
-        value: ''
-    }]
-});
+function MaoEarth() {
 
-$('.standard-entry').on('change', 'input', function() {
-    var jqInput = $(this);
-    if (jqInput.prop('checked')) {
+    var jqContainer = jQuery('#table-wrapper');
+    var jqModal = jQuery('#bang-city');
+    init();
 
-        var entry = jqInput.html();
-        var html = render({
-            name: jqInput.attr('name'),
-            text: jqInput.attr('x-text'),
-            attrs: []
+    var renderTr = _.template(jQuery('#tr').html()); //tr的动态模板
+    var renderTable = _.template(jQuery('#tbody').html()); //拼出表格
+    var render = function recur(table, father) { //递归的拼接tr，计算出正确的id和father
+        return _.reduce(table, function(c, v, k, e) {
+            v.id = _.filter([father, k], function(v) {
+                return v !== null }).join('-');
+            v.father = father;
+            return v.children ? (c + renderTr(v) + recur(v.children, v.id)) :
+                (c + renderTr(v))
+        }, '');
+    };
+    var search = function search(table, id) { //遍历table
+        var container;
+        _.reduce(id.split('-'), function(c, v, k, e) {
+            container = c;
+            c = c[v].children;
+            return c;
+        }, table);
+        return {
+            container: container,
+            model: container[0].model
+        };
+    };
+
+    function render1() {
+        jqContainer.html(renderTable());
+        $("#example-advanced").treetable({
+            expandable: true
         });
-        $('.list-group ').prepend(html);
-    } else {
-    	$('[x-name="'+jqInput.attr('name')+'"]','.list-group').remove();
     }
 
-});
-
-$('#addExt').on('click', function() {
-    var jqInput = $(this);
-    var jqText=jqInput.closest('.col-lg-10').find('input[type="text"]');
-    if (jqText.val()) {
-
-        var entry = jqInput.html();
-        var html = render({
-            name: 'ext',
-            text: jqText.val(),
-            attrs: []
+    function listener() {
+        var id;
+        jqContainer.on('click', '.add', function() {
+            // var 
+            var jqThis = jQuery(this);
+            id = jqThis.attr('x-id')
+            jqModal.modal({
+                title: jqThis.html()
+            });
         });
-        $('.list-group ').prepend(html);
-        jqText.val('');
+
+        jqModal.on('click', '.submit', function() {
+
+            var shape = search(table, id);
+            var row = jQuery.extend(true, {}, shape.model, { name: jQuery('#inputEmail1').val() });
+            shape.container.push(row);
+
+            render1();
+
+        });
     }
 
-});
+    function init() {
+        listener();
+    }
+    return {
+        render: render1
+    }
+}
 
-$('.list-group').on('click','.removeEntry',function(){
-	$(this).parent().remove();
-})
+var maoEarth = new MaoEarth();
+maoEarth.render();
--- src/script/render.js
+++ src/script/render.js
@@ -1,8 +1,22 @@
-var renderTr=_.template(jQuery('#tr').html());
-var renderTable=_.template(jQuery('#tbody').html());
-var render=function recur(table){
-	return _.reduce(table,function(c,v,k,e){
-		return v.children?(c+renderTr(v)+recur(v.children)):
-						  (c+renderTr(v))
-	},'');		
-};
+// var renderTr=_.template(jQuery('#tr').html());//tr的动态模板
+// var renderTable=_.template(jQuery('#tbody').html());//拼出表格
+// var render=function recur(table,father){//递归的拼接tr，计算出正确的id和father
+// 	return _.reduce(table,function(c,v,k,e){
+// 		v.id=_.filter([father,k],function(v){return v!==null}).join('-');
+// 		v.father=father;
+// 		return v.children?(c+renderTr(v)+recur(v.children,v.id)):
+// 						  (c+renderTr(v))
+// 	},'');		
+// };
+// var search=function search(table,id){//遍历table
+// 	var container;
+// 	_.reduce(id.split('-'),function(c,v,k,e){
+// 		container=c;
+// 		c=c[v].children;
+// 		return c;
+// 	},table);
+// 	return {
+// 		container:container,
+// 		model:container[0].model
+// 	};
+// };
